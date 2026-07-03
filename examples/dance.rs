c45130cace012d778fc394bc533d99c9a6e58bc0
Jay <40744358+blamelessgames@users.noreply.github.com>
2026-07-02 19:15:14 -0700

    cleanup and starting an example

---
+++ examples/dance.rs
@@ -0,0 +1,77 @@
+use std::f32::consts::*;
+
+use bevy::{color::palettes::css::*, prelude::*};
+use bevy_ecs_animations::{EntityAnimation, EntityAnimationPlugin, ScaledOutput, ScaledTime};
+
+fn main() -> AppExit {
+    App::new()
+        .add_plugins((DefaultPlugins, EntityAnimationPlugin::<Spinner>::default()))
+        .add_systems(Startup, startup)
+        .run()
+}
+
+fn startup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn((
+        Camera3d::default(),
+        Camera {
+            clear_color: ClearColorConfig::Custom(BLACK.into()),
+            ..default()
+        },
+        Transform::from_translation(vec3(0.0, 0.0, 10.0)).looking_at(Vec3::ZERO, Dir3::Y),
+    ));
+
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: GOLDENROD.into(),
+            unlit: true,
+            ..default()
+        })),
+        Spinner,
+    ));
+}
+
+#[derive(Component)]
+struct Spinner;
+
+impl Spinner {
+    // if you declare curves in const functions that return
+    // `impl Curve<T>` you can ensure cheap inline construction
+    // in the tick function, saving the trouble of trying to write
+    // the insane types combinators will produce
+    const fn axis_curve() -> impl Curve<f32> {
+        // adapt a curve using combinators.
+        // the domain of the curve is the timeline
+        // the output of the curve is whatever you want it to be really
+        ScaledTime::new(
+            0.0,
+            120.0,
+            ScaledOutput::new(-PI, PI, EaseFunction::CircularInOut),
+        )
+    }
+}
+
+impl EntityAnimation for Spinner {
+    type QueryData = &'static mut Transform;
+
+    type QueryFilter = ();
+
+    fn duration(&self) -> f32 {
+        120.0
+    }
+
+    fn tick(&mut self, t: f32, dt: f32, entity: Entity, mut components: Query<&mut Transform>) {
+        let Ok(mut transform) = components.get_mut(entity) else {
+            return;
+        };
+        // do whatever you want, get ticked on schedule til duration is up
+        let axis = Dir3::Y
+            .rotate_z(Spinner::axis_curve().sample_unchecked(t))
+            .normalize();
+        transform.rotate_axis(Dir3::new_unchecked(axis), dt * 10.0);
+    }
+}
--- src/combinators.rs
+++ src/combinators.rs
@@ -6,7 +6,7 @@ use bevy_math::{Curve, FloatExt, curve::Interval};
 // adjust the second's domain to the end of the first, i.e. require it
 // to start at zero, or if they are responsible for time and we just hold
 #[derive(Clone, Copy)]
-pub struct Seq<T, C1: Curve<T>, C2: Curve<T>> {
+pub struct Seq<T, C1, C2> {
     first_curve: C1,
     second_curve: C2,
     _marker: PhantomData<T>,
@@ -47,7 +47,7 @@ impl<T, C1: Curve<T>, C2: Curve<T>> Curve<T> for Seq<T, C1, C2> {
 }
 
 /// delays the underlying curve by `delay` seconds
-pub struct Delay<T, C: Curve<T>> {
+pub struct Delay<T, C> {
     delay: f32,
     curve: C,
     _marker: PhantomData<T>,
@@ -87,25 +87,25 @@ impl<T, C: Curve<T>> Curve<T> for Delay<T, C> {
 
 // scales the output. only works when the output is
 // f32, if i need more i'll bring in num traits
-pub struct ScaledOutput<C: Curve<f32>> {
-    low: f32,
-    high: f32,
+pub struct ScaledOutput<T, C> {
+    low: T,
+    high: T,
     curve: C,
 }
 
-impl<C: Curve<f32>> ScaledOutput<C> {
-    pub const fn new(low: f32, high: f32, curve: C) -> Self {
+impl<T, C: Curve<T>> ScaledOutput<T, C> {
+    pub const fn new(low: T, high: T, curve: C) -> Self {
         ScaledOutput { low, high, curve }
     }
 }
 
-impl<C: Curve<f32>> From<(f32, f32, C)> for ScaledOutput<C> {
+impl<C: Curve<f32>> From<(f32, f32, C)> for ScaledOutput<f32, C> {
     fn from((low, high, curve): (f32, f32, C)) -> Self {
         Self { low, high, curve }
     }
 }
 
-impl<C: Curve<f32>> Curve<f32> for ScaledOutput<C> {
+impl<C: Curve<f32>> Curve<f32> for ScaledOutput<f32, C> {
     fn domain(&self) -> Interval {
         self.curve.domain()
     }
@@ -118,16 +118,17 @@ impl<C: Curve<f32>> Curve<f32> for ScaledOutput<C> {
 /// scales the underlying curve so its domain is exposed
 /// as low->high instead. like if you just want to add
 /// an easing function for a stretch in a sequence
-pub struct TimeScaled<T, C: Curve<T>> {
+pub struct ScaledTime<T, C> {
     low: f32,
     high: f32,
     curve: C,
     _marker: PhantomData<T>,
 }
 
-impl<T, C: Curve<T>> TimeScaled<T, C> {
+impl<T, C: Curve<T>> ScaledTime<T, C> {
     pub const fn new(low: f32, high: f32, curve: C) -> Self {
-        TimeScaled {
+        debug_assert!(low < high && low >= 0.0);
+        ScaledTime {
             low,
             high,
             curve,
@@ -136,7 +137,7 @@ impl<T, C: Curve<T>> TimeScaled<T, C> {
     }
 }
 
-impl<T, C: Curve<T>> From<(f32, f32, C)> for TimeScaled<T, C> {
+impl<T, C: Curve<T>> From<(f32, f32, C)> for ScaledTime<T, C> {
     fn from((low, high, curve): (f32, f32, C)) -> Self {
         debug_assert!(low < high && low >= 0.0);
         Self {
@@ -148,7 +149,7 @@ impl<T, C: Curve<T>> From<(f32, f32, C)> for TimeScaled<T, C> {
     }
 }
 
-impl<T, C: Curve<T>> Curve<T> for TimeScaled<T, C> {
+impl<T, C: Curve<T>> Curve<T> for ScaledTime<T, C> {
     fn domain(&self) -> Interval {
         Interval::new(self.low, self.high).unwrap()
     }
